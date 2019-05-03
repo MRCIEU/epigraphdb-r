a20b80f7cc3606a0b907561af13a3b8bda367228
YiLiu6240 <yi.liu6240@gmail.com>
2019-05-03 16:11:16 +0100

    Switch to internal api

--- R/zzz.R
+++ R/zzz.R
@@ -11,7 +11,8 @@
 .onLoad <- function(libname, pkgname) {
   op <- options()
   op.epigraphdb <- list(
-    epigraphdb.api.url = "http://app-dc1-epigdb-p0.epi.bris.ac.uk:8035"
+    # epigraphdb.api.url = "http://app-dc1-epigdb-p0.epi.bris.ac.uk:8035"
+    epigraphdb.api.url = "http://jojo.epi.bris.ac.uk:8117"
   )
   toset <- !(names(op.epigraphdb) %in% names(op))
   if (any(toset)) options(op.epigraphdb[toset])
